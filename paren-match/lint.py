988e88e43c02d661ae13369226950b1a2b44e07a
Guanran Tai <taiguanran@gmail.com>
2024-07-13 22:26:56 +0200

    Add my pares match

---
+++ paren-match/lint.py
@@ -0,0 +1,56 @@
+def lint(input_file: str) -> None:
+    left_pares = ["(", "[", "{"]
+    right_pares = [")", "]", "}"]
+    pares = []
+    with open(input_file, "r") as f:
+        for line_no, line in enumerate(f):
+            for char in line:
+                if char in left_pares:
+                    pares.append(left_pares.index(char))
+                if char in right_pares:
+                    if len(pares) == 0:
+                        print(f"Face not matched pares on line {line_no} char {char}")
+                    else:
+                        expected_char = right_pares[pares.pop()]
+                        if expected_char != char:
+                            print(
+                                f"Face not matched pares on line {line_no} char {char} expected {expected_char}"
+                            )
+                        else:
+                            pass
+        if len(pares) != 0:
+            print(f"Not empty stack {pares}")
+
+
+def pare_match(str):
+    left_pares = ["(", "[", "{"]
+    right_pares = [")", "]", "}"]
+    pares = []
+    for char in str:
+        if char in left_pares:
+            pares.append(left_pares.index(char))
+        if char in right_pares:
+            if len(pares) == 0:
+                return False
+            else:
+                expected_char = right_pares[pares.pop()]
+                if expected_char != char:
+                    return False
+    return len(pares) == 0
+
+
+def test_matched_cases():
+    assert pare_match("[]") is True
+    assert pare_match("()") is True
+    assert pare_match("{}") is True
+    assert pare_match("{()}") is True
+    assert pare_match("{([])}") is True
+    assert pare_match("{]") is False
+    assert pare_match("({}]") is False
+        
+    print("All Test passed")
+
+
+if __name__ == "__main__":
+    test_matched_cases()
+    # lint("./stretch.rkt")
